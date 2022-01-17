5d9fd713dd04f25853e0ba80d754956d1ec11cdf
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-01-17 18:54:47 +0530

    maybe the last

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const pool= mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'blood_details',
-    password: 'password'
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b5e4294f3aef51',
+    database: 'heroku_3caabb8df55adbb',
+    password: '4a8d7be3'
 
 })
 module.exports =pool.promise()
